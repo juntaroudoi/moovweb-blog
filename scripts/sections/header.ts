1d29088337e31e58b803a2c73731a561a64378c9
Gregory G. Tseng <gregory.tseng@moovweb.com>
2013-06-10 17:41:09 -0700

    fixed image link

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -1,7 +1,7 @@
 $$("header") { # select elements with CSS: scope into header element
   $$("strong.logo.vcard") { # select elements with CSS: In previous scope, scope into strong element with classes of 'logo' and 'vcard' <strong class="logo vcard">...</strong>
     inner("") # empties contents of this node (sets node to empty string "")
-    insert("img", src: "https:#s3.amazonaws.com/moovweb-apollo/logo.png") # insert <img> element with src attribute
+    insert("img", src: "https://s3.amazonaws.com/moovweb-apollo/logo.png") # insert <img> element with src attribute
   } # end <strong class="logo vcard"> scope
   $$("form.form-search") { # select elements with CSS: in previous scope, scope into form element with class 'form-search' <form class="form-search">...</form>
     $$("input") { # select elements with CSS: in previous scope, scope into all input elements
